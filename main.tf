ee5a3254a20af2be9f9f627174ba0e8237ab5303
anshdesai04 <ahdesai@ad.unc.edu>
2025-06-21 12:52:22 -0400

    feat: Provision VPC, subnet, and EC2 instance

--- main.tf
+++ main.tf
@@ -11,3 +11,76 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Networking
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+      Name = "main-vpc"
+    }
+}
+
+resource "aws_subnet" "public" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    
+    tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+    vpc_id = aws_vpc.main.id
+    
+    tags = {
+      Name = "main-igw"
+    }
+}
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+
+    tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+    subnet_id = aws_subnet.public.id
+    route_table_id = aws_route_table.public.id
+}
+# --- EC2 INSTANCE ---
+
+# Find the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Create a web server
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro" # This is eligible for the AWS Free Tier
+  subnet_id = aws_subnet.public.id
+
+  tags = {
+    Name = "Cloud-Incident-Sim-Server"
+  }
+}
