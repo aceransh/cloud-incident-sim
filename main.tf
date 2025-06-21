713e8ac303d437ecec7b5b8709736c5604e52457
anshdesai04 <ahdesai@ad.unc.edu>
2025-06-21 13:11:08 -0400

    feat: Add security group for web server

--- main.tf
+++ main.tf
@@ -56,6 +56,32 @@ resource "aws_route_table_association" "public" {
     subnet_id = aws_subnet.public.id
     route_table_id = aws_route_table.public.id
 }
+
+# Security Group
+resource "aws_security_group" "web_server_sg" {
+  name        = "web-server-sg"
+  description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from my IP"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["136.47.155.189/32"] # <-- IMPORTANT: REPLACE THIS
+  }
+
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # "-1" means all protocols
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    tags = {
+    Name = "web-server-sg"
+  }
+}
 # --- EC2 INSTANCE ---
 
 # Find the latest Amazon Linux 2 AMI
@@ -79,6 +105,7 @@ resource "aws_instance" "web_server" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro" # This is eligible for the AWS Free Tier
   subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
 
   tags = {
     Name = "Cloud-Incident-Sim-Server"
